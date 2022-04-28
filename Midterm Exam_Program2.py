b21f8bcb73ab53925de58b2aace04525b8295cee
Gimarose <92917121+Gimarose@users.noreply.github.com>
2022-04-28 14:50:23 +0800

    Add files via upload

---
+++ Midterm Exam_Program2.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+window = Tk()
+
+window.title("Special MidTerm Exam in OOP")
+window.geometry("500x400+20+10")
+
+btn = Button(window, text = "Click to Change Color")
+btn.place(x= 180, y=170)
+btn.config(bg='white')
+btn.config(fg='black')
+btn.config(activebackground='yellow')
+
+window.mainloop()
+
